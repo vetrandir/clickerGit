6030db910c34ee700a2cbe815329d8e03fbde097
vetrandir <vetrandir@gmail.com>
2018-08-21 21:38:17 +0300

    TestAnother

---
+++ app/src/main/java/com/example/vetrandir/clicker/TestAnother.java
@@ -0,0 +1,13 @@
+package com.example.vetrandir.clicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestAnother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_another);
+    }
+}
